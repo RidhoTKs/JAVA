80cdbc976be22a96d69d62c3a3e742ca736cfb9e
RidhoTKs <ridhotks@gmail.com>
2021-08-02 12:09:26 +0700

    menambahkan exercise untuk materi while and do while

---
+++ looping/WhileLoopExercise/src/com/example/SquareRootWhile.java
@@ -0,0 +1,21 @@
+
+package com.example;
+
+import java.util.Scanner;
+
+
+public class SquareRootWhile {
+    public static void main(String args[])
+    {
+    System.out.print("Type a non-negative integer: ");
+     Scanner console = new Scanner(System.in);
+	int number = console.nextInt();
+        while(number < 0){
+            System.out.println("invalid number " + number);
+            number = console.nextInt();
+        }
+        System.out.println("the square root of " + number + " is " + Math.sqrt(number));
+    
+}
+    
+}
